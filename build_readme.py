2f83d7613fb58aa80b2d06ecc8c3bb2a6ce5e981
Dat Adithya <31721284+dat-adi@users.noreply.github.com>
2020-12-10 21:38:01 +0530

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -4,9 +4,8 @@ import json
 import sys
 import re
 import os
+from bs4 import BeautifulSoup
 
-# https://help.medium.com/hc/en-us/articles/214874118-RSS-feeds
-# https://github.com/skolakoda/programming-quotes-api 👏
 
 root = pathlib.Path(__file__).parent.resolve()
 
@@ -24,47 +23,26 @@ def replace_chunk(content, marker, chunk, inline=False):
 	# replace matched string using pattern provided with the chunk
 	return r.sub(chunk, content)
 
-def fetch_blog_posts(link):
-	result = []
-	response = requests.get(link)
-	if response.status_code == 200:
-		posts = json.loads(response.text)["items"]
-		for post in posts:
-			# skip the comments
-			if len(post["categories"]) != 0:
-				post["pubDate"] = post["pubDate"].split()[0]
-				result.append(post)
-	elif response.status_code == 404:
-		print('Not Found: ') + link
-	return result
+def fetch_article_links(link):
+	page = requests.get(link).text
+    soup = BeautifulSoup(page, "html.parser")
+    articles_tag = soup.find_all("a", "author--post")
+    article_links = [link.get("href") for link in articles_tag]
+	return article_links
 
-def fetch_programming_quotes(link):
-	result = ""
-	response = requests.get(link)
-	if response.status_code == 200:
-		posts = json.loads(response.text)
-		if "en" in posts and "author" in posts:
-			result = "“*{}*”<br/>— **{}**".format(posts["en"], posts["author"])
-		else:
-			result = "{} -- {}".format("Simplicity is prerequisite for reliability.", "Edsger W. Dijkstra")
-	elif response.status_code == 404:
-		print('Not Found: ') + link
-	return result
 
 if __name__ == "__main__":
 	readme = root / "README.md"
 
 	readme_contents = readme.open().read()
 	rewritten = readme_contents
-	rewritten = replace_chunk(rewritten, "programming-quote", fetch_programming_quotes("https://programming-quotes-api.herokuapp.com/quotes/random"))
 
-	posts = fetch_blog_posts("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@lifeparticle")
+	posts = fetch_article_links("https://www.askpython.com/author/datta")
 	if len(posts) != 0:
-		# markdown formatting
 		posts_md = "\n".join(
-			["* [{title}]({link}) <br/> <sub>{pubDate}</sub>".format(**post) for post in posts]
+			["{} <br/>".format(link) for link in posts]
 		)
 
-		rewritten = replace_chunk(rewritten, "blog", posts_md)
+		rewritten = replace_chunk(rewritten, "article-links", posts_md)
 
 	readme.open("w").write(rewritten)
